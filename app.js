e2ef74151cafee094ede22b1c3059e34dd488047
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-11-21 13:11:10 -0500

    Removed comments

--- app.js
+++ app.js
@@ -11,36 +11,3 @@ require('./routes.js')(app, bandcamp);
 http.listen(8000, function(){
   console.log('listening on *:8000');
 });
-
-//function getRandomInt (min, max) {
-//    return Math.floor(Math.random() * (max - min + 1)) + min;
-//}
-//
-//var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//
-//var length = getRandomInt(3, 7);
-//
-//var query = ""
-//for (var i = 0; i < length; i++){
-//  query += letters.charAt(getRandomInt(0, letters.length));
-//}
-// 
-//var params = {
-//  query: query,
-//  page: 1
-//};
-//
-//var album = ""
-// 
-//bandcamp.search(params, function(error, searchResults) {
-//  if (error) {
-//    console.log(error);
-//  } else {
-//    for (var i = 0; i < searchResults.length; i++){
-//      if (album == "" && searchResults[i].type == "album") {
-//        album = searchResults[i].url;
-//        open(album);
-//      }
-//    }
-//  }
-//});
