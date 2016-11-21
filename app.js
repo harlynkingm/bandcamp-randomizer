a0b32c5956486cebda6c38d77e2e41802cd2bc91
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-11-21 13:19:59 -0500

    Changed server routing

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var bandcamp = require('bandcamp-scraper');
 app.use(morgan('dev'));
 require('./routes.js')(app, bandcamp);
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+app.listen(app.get('port') ,app.get('ip'), function () {
+    console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
